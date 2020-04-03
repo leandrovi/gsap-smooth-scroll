7bfb090f5e99a5bf3962ac92a543a36b619b79f2
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-03 18:16:16 -0300

    feat: add project to github

---
+++ index.js
@@ -0,0 +1,96 @@
+var html = document.documentElement;
+var body = document.body;
+
+var scroller = {
+  target: document.querySelector("#scroll-container"),
+  ease: 0.05, // <= scroll speed
+  endY: 0,
+  y: 0,
+  resizeRequest: 1,
+  scrollRequest: 0,
+};
+
+var requestId = null;
+
+TweenLite.set(scroller.target, {
+  rotation: 0.01,
+  force3D: true,
+});
+
+window.addEventListener("load", onLoad);
+
+function onLoad() {
+  updateScroller();
+  window.focus();
+  window.addEventListener("resize", onResize);
+  document.addEventListener("scroll", onScroll);
+}
+
+function updateScroller() {
+  var resized = scroller.resizeRequest > 0;
+
+  if (resized) {
+    var height = scroller.target.clientHeight;
+    body.style.height = height + "px";
+    scroller.resizeRequest = 0;
+  }
+
+  var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+  scroller.endY = scrollY;
+  scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+  if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+    scroller.y = scrollY;
+    scroller.scrollRequest = 0;
+  }
+
+  TweenLite.set(scroller.target, {
+    y: -scroller.y,
+  });
+
+  requestId =
+    scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+}
+
+function onScroll() {
+  scroller.scrollRequest++;
+  if (!requestId) {
+    requestId = requestAnimationFrame(updateScroller);
+  }
+}
+
+function onResize() {
+  scroller.resizeRequest++;
+  if (!requestId) {
+    requestId = requestAnimationFrame(updateScroller);
+  }
+}
+
+// Reveal on Scroll
+const items = document.querySelectorAll(".item");
+
+const config = {
+  threshold: 0.5,
+};
+
+const tl = new TimelineMax();
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      let overlap = "-=0.3";
+
+      if (!tl.isActive()) {
+        overlap = "+=0";
+      }
+
+      tl.to(entry.target, 0.5, { autoAlpha: 1 }, overlap);
+      self.unobserve(entry.target);
+    }
+  });
+}, config);
+
+items.forEach((box) => {
+  observer.observe(box);
+});
